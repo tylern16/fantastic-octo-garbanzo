803e8e4b447750c8eb8ed77e2ecfb7d3599d7021
Tyler Nenninger <tylernenninger@Annas-Air.lan>
2022-11-07 18:29:04 -0600

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react'
+
 
 function App() {
+
+  let [team, setTeam] = useState()
+
+  let myHeaders = new Headers();
+  myHeaders.append("x-rapidapi-key", "63aafd4c3e8fcdca734f5eb313759249");
+  myHeaders.append("x-rapidapi-host", "v3.football.api-sports.io");
+
+  let requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
+  
+  const getTeam = () => {
+    fetch("https://v3.football.api-sports.io/teams?id=33", requestOptions)
+    .then(response => response.json())
+    .then(result => {
+      console.log(result.response[0].team, "result")
+      setTeam(result.response[0].team)
+    })
+    .catch(error => console.log('error', error));
+  }
+
+  useEffect(()=>{
+    getTeam()
+  },[])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Footballr</h1>
+      <ul>
+        <li>{team.name}</li>
+        <img src={team.logo}/> 
+        <li>{team.founded}</li> 
+      </ul>  
+
     </div>
   );
 }
